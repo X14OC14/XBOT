5c72356615368a7ec4f9ea770629c8499e8b3a6e
X I Λ O C I Λ <xiaocia@outlook.co.id>
2024-07-16 16:28:13 +0700

    Update _openai-ai-logic.js

--- plugins/_openai-ai-logic.js
+++ plugins/_openai-ai-logic.js
@@ -7,7 +7,7 @@ var handler = async (m, {
  }) => {
 if (!text) throw `Masukkan pertanyaan!\n\n*Contoh:* Siapa Kamu? `
 //Set Logic Disini 
-let logic = 'Hai Saya Adalah BetaBotz-Md Bot Whatsapp Yang Dikembangkan Oleh Lann,Saya Bernama Betabotz-Md,Saya Dibuat Oleh Lann Dengan Penuh Kesempurnaan Yang Tiada Taraa,Jika Kamu Ingin Mencari Tau Lebih Dalam Tentang Ownerku Visit https://api.betabotz.org'
+let logic = 'Hai Saya Adalah XIAOCIA-Bot Whatsapp Yang Dikembangkan Oleh Mafy, Saya Bernama XIAOCIA, Saya Dibuat Oleh Mafy Dengan Penuh Kesempurnaan Yang Tiada Taraa,Jika Kamu Ingin Mencari Tau Lebih Dalam Tentang Ownerku Visit https://xiaocia.xyz atau hubungi langsung dia dengan mengetik .owner'
 await m.reply(wait)
   var js = await fetch(`https://api.betabotz.eu.org/api/search/openai-logic?text=${text}&logic=${logic}&apikey=${lann}`)
 var json = await js.json()
@@ -16,7 +16,7 @@ try {
 } catch (err ) {
 m.reply(`${eror}`)
 }}
-handler.command = handler.help = ['ai2','openai2','chatgpt2'];
+handler.command = handler.help = ['ai','openai','chatgpt'];
 handler.tags = ['info'];
 handler.premium = false
 module.exports = handler;
